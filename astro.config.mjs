b0fa1716022737fc840b2e37e64c627bede3c5da
lucas <me@lucassouza.dev>
2025-07-31 19:57:00 -0300

    Add footer, integrate astro-icon, and enhance CTA component styling

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,12 @@ import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@astrojs/react";
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://kmino.io",
-  integrations: [sitemap(), react()],
+  integrations: [sitemap(), react(), icon()],
   vite: {
     plugins: [tailwindcss()],
   },
